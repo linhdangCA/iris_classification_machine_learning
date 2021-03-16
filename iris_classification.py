7ddd34167514a3eab57ae791a7938b2fc52e1dcb
Linh Dang <linh.dang01@gmail.com>
2021-03-16 15:14:22 -0700

    removed some duplicate code

--- iris_classification.py
+++ iris_classification.py
@@ -38,16 +38,7 @@ dataset = read_csv(url, names=names)
 # class distribution
 # print(dataset.groupby('class').size())
 
-from matplotlib import pyplot
 # visualize the data
-from pandas import read_csv
-from pandas.plotting import scatter_matrix
-
-# Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = read_csv(url, names=names)
-
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 pyplot.show()
